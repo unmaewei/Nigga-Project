c27e0dc0d200801b6b500cf96711e71d6c3b683d
unmaewei <75463038+unmaewei@users.noreply.github.com>
2021-01-13 15:12:51 -0500

    Create nigga.cpp

---
+++ nigga.cpp
@@ -0,0 +1,471 @@
+//
+// nigga includes
+//
+
+#include <iostream>
+#include <Windows.h>
+
+//
+// nigga defines
+//
+
+#define nigga_comma ,
+#define nigga_angle_bracket_start <
+#define nigga_angle_bracket_end >
+#define nigga_parenthesis_start (
+#define nigga_parenthesis_end )
+#define nigga_semi_colon ;
+#define nigga_start
+#define nigga_end
+#define nigga_two_colons  :: 
+
+//
+// end nigga degines
+//
+
+//
+//nigga space
+//
+
+//
+// nigga class
+//
+namespace nigga
+{
+	//
+	// nigga memory
+	//
+	namespace nigga_memory
+	{
+		//
+		// nigga read
+		//
+		template<typename t>
+		uintptr_t nigga_read(uintptr_t n)
+		{
+			//
+			// nigga space
+			//
+		}
+	}
+
+	//
+	// nigga types
+	//
+	namespace nigga_types
+	{
+		//
+		// nigga byte
+		//
+		using nigga_byte = byte;
+		//
+		// nigga memory : credits bright#2203
+		//
+		using memory = nigga_byte;
+	}
+
+	//
+	// nigga game
+	//
+	namespace nigga_game
+	{
+		//
+		// nigga base
+		//
+		uintptr_t nigga_base;
+	}
+
+	//
+	// nigga offsets
+	//
+	namespace nigga_offsets
+	{
+		//
+		// nigga round
+		//
+		uintptr_t nigga_round;
+	}
+
+	//
+	// nigga utils
+	//
+	namespace nigga_utils
+	{
+		//
+		// nigga add
+		//
+		uintptr_t nigga_add(uintptr_t p, uintptr_t pp)
+		{
+			//
+			// nigga return
+			//
+			return p + pp;
+		}
+
+		//
+		// nigga aquire
+		//
+		uintptr_t nigga_aquire(uintptr_t ppp)
+		{
+			//
+			// nigga return
+			//
+			return ppp;
+		}
+	}
+
+	//
+	// nigga memory v2
+	//
+	namespace nigga_mememoryv2
+	{
+		//
+		// nigga rpm
+		//
+		template<typename t>
+		uintptr_t rpm(uintptr_t n)
+		{
+			//
+			// nigga space
+			//
+		}
+	}
+}
+
+//
+// end nigga
+//
+
+//
+// nigga space
+//
+
+//
+// nigga main
+//
+
+int main()
+{
+
+//
+// start nigga
+//
+
+	nigga_start
+		//
+		// nigga function
+		//
+	 nigga_parenthesis_start
+//
+// nigga function
+//
+		nigga nigga_two_colons
+		//
+		// nigga function
+		//
+			nigga_mememoryv2 
+		//
+		// nigga function
+		//
+				nigga_two_colons 
+		//
+		// nigga function
+		//
+					rpm
+		//
+		// nigga function
+		//
+						nigga_angle_bracket_start
+		//
+		// nigga function
+		//
+							nigga 
+		//
+		// nigga function
+		//
+								nigga_two_colons 
+		//
+		// nigga function
+		//
+									nigga_types 
+		//
+		// nigga function
+		//
+										nigga_two_colons 
+		//
+		// nigga function
+		//
+											memory
+		//
+		// nigga function
+		//
+												nigga_angle_bracket_end 
+		//
+		// nigga function
+		//
+													nigga_parenthesis_start
+		//
+		// nigga function
+		//
+														nigga 
+		//
+		// nigga function
+		//
+															nigga_two_colons 
+		//
+		// nigga function
+		//
+																nigga_utils 
+		//
+		// nigga function
+		//
+																	nigga_two_colons 
+		//
+		// nigga function
+		//
+																		nigga_aquire 
+		//
+		// nigga function
+		//
+																			nigga_parenthesis_start
+		//
+		// nigga function
+		//
+																				nigga 
+		//
+		// nigga function
+		//
+																					nigga_two_colons 
+		//
+		// nigga function
+		//
+																						nigga_memory 
+		//
+		// nigga function
+		//
+																							nigga_two_colons 
+		//
+		// nigga function
+		//
+																								nigga_read 
+		//
+		// nigga function
+		//
+																									nigga_angle_bracket_start
+		//
+		// nigga function
+	    //
+																										nigga 
+		//
+		// nigga function
+		//
+																											nigga_two_colons 
+				//
+				// nigga function
+				//
+																												nigga_types 
+					//
+					// nigga function
+					//
+																													nigga_two_colons 
+						//
+						// nigga function
+						//
+																														nigga_byte 
+							//
+							// nigga function
+							//
+																															nigga_angle_bracket_end 
+								//
+								// nigga function
+								//
+																																nigga_parenthesis_start
+									//
+									// nigga function
+									//
+																																	nigga 
+//
+// nigga function
+//
+																																		nigga_two_colons 
+//
+// nigga function
+//
+																																			nigga_utils 
+//
+// nigga function
+//
+																																				nigga_two_colons 
+													//
+													// nigga function
+													//
+																																					nigga_aquire 
+														//
+														// nigga function
+														//
+																																						nigga_parenthesis_start
+															//
+															// nigga function
+															//
+																																							nigga 
+																//
+																// nigga function
+																//
+																																								nigga_two_colons 
+																	//
+																	// nigga function
+																	//
+																																									nigga_utils 
+																		//
+																		// nigga function
+																		//
+																																										nigga_two_colons 
+																			//
+																			// nigga function
+																			//
+																																											nigga_add
+																				//
+																				// nigga function
+																				//
+																																												nigga_parenthesis_start
+																					//
+																					// nigga function
+																					//
+																																													nigga 
+																						//
+																						// nigga function
+																						//
+																																														nigga_two_colons 
+																							//
+																							// nigga function
+																							//
+																																															nigga_utils 
+																								//
+																								// nigga function
+																								//
+																																																nigga_two_colons 
+																									//
+																									// nigga function
+																									//
+																																																	nigga_aquire 
+																										//
+																										// nigga function
+																										//
+																																																		nigga_parenthesis_start
+																											//
+																											// nigga function
+																											//
+																																																			nigga 
+																												//
+																												// nigga function
+																												//
+																																																				nigga_two_colons 
+																													//
+																													// nigga function
+																													//
+																																																					nigga_game 
+																														//
+																														// nigga function
+																														//
+																																																						nigga_two_colons 
+																															//
+																															// nigga function
+																															//
+																																																							nigga_base
+																																//
+																																// nigga function
+																																//
+																																																								nigga_parenthesis_end 
+																																	//
+																																	// nigga function
+																																	//
+																																																									nigga_comma
+																																		//
+																																		// nigga function
+																																		//
+																																																										nigga 
+																																			//
+																																			// nigga function
+																																			//
+																																																											nigga_two_colons
+																																				//
+																																				// nigga function
+																																				//
+																																																												nigga_utils 
+																																					//
+																																					// nigga function
+																																					//
+																																																													nigga_two_colons 
+																																						//
+																																						// nigga function
+																																						//
+																																																														nigga_aquire 
+																																							//
+																																							// nigga function
+																																							//
+																																																															nigga_parenthesis_start
+																																								//
+																																								// nigga function
+																																								//
+																																																																nigga 
+																																									//
+																																									// nigga function
+																																									//
+																																																																	nigga_two_colons 
+																																										//
+																																										// nigga function
+																																										//
+																																																																		nigga_offsets 
+																																											//
+																																											// nigga function
+																																											//
+																																																																			nigga_two_colons 
+																																												//
+																																												// nigga function
+																																												//
+																																																																				nigga_round
+																																													//
+																																													// nigga function
+																																													//
+//
+// nigga parenthesis end
+//	
+									nigga_parenthesis_end
+//
+// nigga parenthesis end
+//
+								nigga_parenthesis_end
+//
+// nigga parenthesis end
+//
+							nigga_parenthesis_end
+//
+// nigga parenthesis end
+//
+						nigga_parenthesis_end
+//
+// nigga parenthesis end
+//
+					nigga_parenthesis_end
+//
+// nigga parenthesis end
+//
+				nigga_parenthesis_end 
+//
+// nigga parenthesis end
+//
+			nigga_parenthesis_end
+//
+// nigga parenthesis end
+//
+		nigga_semi_colon
+ //
+// nigga parenthesis end
+//
+	nigga_end
+
+	//
+	// end nigga
+	//
+}
+
